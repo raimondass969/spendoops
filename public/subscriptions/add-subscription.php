8e4192d86020a1873ae0a99ed3e2380cccfe355f
Raimondas <raimondas.satkunas@gmail.com>
2026-09-18 17:22:09 +0300

    Fix subscription request validation

--- public/subscriptions/add-subscription.php
+++ public/subscriptions/add-subscription.php
@@ -7,11 +7,28 @@ const ADD_SUB_FORM = 'Location: add-subscription-form.php';
 
 $pdo = require_once __DIR__ . '/../../bootstrap.php';
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    Header('Location: add-subscription-form.php');
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true || !isset($_SESSION['user_id'])) {
+    $_SESSION['error_message'] = 'Not logged in.';
+    Header('Location: ../auth/login-form.php');
     exit();
 }
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    Header(ADD_SUB_FORM);
+    exit();
+}
+
+if (!isset(
+    $_POST['subscription_name'],
+    $_POST['subscription_price'],
+    $_POST['subscription_billing_cycle'],
+    $_POST['subscription_next_payment_date'],
+    $_POST['CSRF_TOKEN'],
+)) {
+    $_SESSION['error_message'] = 'Missing required data.';
+    header(ADD_SUB_FORM);
+    exit();
+}
 
 $subscriptionName = $_POST['subscription_name'];
 $subscriptionPrice = $_POST['subscription_price'];
@@ -22,7 +39,7 @@ $userId = $_SESSION['user_id'];
 
 if (!Csrf::validateToken($token)) {
     $_SESSION['error_message'] = 'Invalid CSRF token';
-    header('Location: ADD_SUB_FORM');
+    header(ADD_SUB_FORM);
     exit();
 }
 
